ada8e0eee869bd4c39c56a3962d7ae5d841ba014
komalkanth <>
2023-09-21 18:21:05 +0530

    organizing the folders

--- regions/us-east-1/vpc/vpc-1/main.tf
+++
@@ -1,7 +0,0 @@
-module "vpc" {
-  source        = "../../../../../aws-tf-modules/vpc/"
-  region        = var.region
-  vpc_main_cidr = "10.1.0.0/16"
-  subnet_cidr   = ["10.1.0.0/24", "10.1.1.0/24"]
-}
-
