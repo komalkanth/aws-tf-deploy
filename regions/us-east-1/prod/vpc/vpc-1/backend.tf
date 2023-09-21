54b35aa6beb64a8e2efe73656d2096ab8a046e0a
komalkanth <>
2023-09-21 19:00:23 +0530

    added s3 backend

---
+++ regions/us-east-1/prod/vpc/vpc-1/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket  = "saac03generalstatebucket"
+    key     = "regions/us-east-1/prod/vpc/vpc-1/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "iamadmin-general"
+
+    dynamodb_table = "tfstatelock"
+  }
+}
