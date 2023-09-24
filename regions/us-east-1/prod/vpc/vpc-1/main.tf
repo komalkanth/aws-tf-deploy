683e4deb402520b52ffad38367979738d92a5784
komalkanth <>
2023-09-24 22:34:37 +0530

    terraform fmt

--- regions/us-east-1/prod/vpc/vpc-1/main.tf
+++ regions/us-east-1/prod/vpc/vpc-1/main.tf
@@ -6,25 +6,25 @@ module "vpc" {
   organization = "kkoncloud.com"
   environment  = "prod"
 
-  vpc_name            = "vpc-1"
-  vpc_main_cidr       = "10.1.0.0/16"
-  public_subnet_cidr_map  = {
-    "us-east-1a": {
-      "subnet-1": "10.1.0.0/24"
+  vpc_name      = "vpc-1"
+  vpc_main_cidr = "10.1.0.0/16"
+  public_subnet_cidr_map = {
+    "us-east-1a" : {
+      "subnet-1" : "10.1.0.0/24"
     }
-    "us-east-1b": {
-      "subnet-1": "10.1.1.0/24"
-      }
-    "us-east-1c": {
-      "subnet-1": "10.1.3.0/24"
-      "subnet-2": "10.1.4.0/24"
-      }
+    "us-east-1b" : {
+      "subnet-1" : "10.1.1.0/24"
+    }
+    "us-east-1c" : {
+      "subnet-1" : "10.1.3.0/24"
+      "subnet-2" : "10.1.4.0/24"
+    }
   }
   private_subnet_cidr_map = {
-      "us-east-1a": {
-      "subnet-1": "10.1.2.0/24"
+    "us-east-1a" : {
+      "subnet-1" : "10.1.2.0/24"
     }
-    }
+  }
 }
 
 output "locals_output" {
